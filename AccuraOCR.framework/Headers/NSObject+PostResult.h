23842d00da69f486fd9be45cc7a116d855adc100
azhar0904 <93505171+azhar0904@users.noreply.github.com>
2022-03-31 16:10:25 +0530

    Add glare func in framework

--- AccuraOCR.framework/Headers/NSObject+PostResult.h
+++ AccuraOCR.framework/Headers/NSObject+PostResult.h
@@ -8,15 +8,11 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "VideoCameraWrapperDelegate.h"
 
 
 
 
 @interface PostResult: NSObject
-{
-    ContentType contentType;
-}
 
 typedef void (^SuccessBlock)(NSMutableDictionary* Response);
 typedef void (^FailureBlock)(NSError* error);
